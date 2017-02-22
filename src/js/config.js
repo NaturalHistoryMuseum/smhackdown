04b70e88c32aca1d7ff7e789a355b1b5f4669c5d
benscott <ben@benscott.co.uk>
2017-02-22 12:31:22 +0000

    Switch to live API

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // api: 'https://quiet-thicket-98438.herokuapp.com/api',
-  api: 'http://127.0.0.1:5000/api'
+  api: 'https://quiet-thicket-98438.herokuapp.com/api',
+  // api: 'http://127.0.0.1:5000/api'
 }
 
 
